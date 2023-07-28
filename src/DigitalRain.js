0e74bb008131c63f538ab53ac39e2630080213f0
nohren <orendnelson@gmail.com>
2023-07-27 23:31:19 -0700

    2.2.1

--- src/DigitalRain.js
+++ src/DigitalRain.js
@@ -19,13 +19,11 @@ const generateBlob = async (url) => {
 
 const exitFullScreen = () => screenfull.exit();
 
-
-
 /**
  * A simple component that fits the container given
  * Full screen mode on click: boolean
  */
-export default function DigitalRain({
+export function DigitalRain({
   fullScreen = false
 }) {
   //forces a render on tab change, so we don't lose timing
